acd741c354f029abf851f8f5e6c22189f6c8c9c9
ajiekss <61558976+ajiekss@users.noreply.github.com>
2022-11-14 15:38:25 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Session Limit | Kursus Web YELLOWWEB.ID</title>
+</head>
+<body>
+
+	<h3>Halaman tanpa sesi</h3>
+	<a href="limit_ses.php">Klik disini untuk menuju halaman dengan mengaktifkan sesi</a>
+
+</body>
+</html>
